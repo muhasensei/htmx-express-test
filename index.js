586fd9b4d4dc557a7da19b627a8ba61c2359f4d3
m.zhanuzakov <m.zhanuzakov@jusaninvest.kz>
2023-09-12 18:52:06 +0600

    some other tests

--- index.js
+++ index.js
@@ -13,19 +13,32 @@ const Handlebars = TemplatingEngine.create({
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static('public'))
 app.engine('html', Handlebars.engine);
 app.set('view engine', 'handlebars');
 
+let name = 'name';
+
 app.get('/', (req, res) => {
-    res.render('index.html', {name: 'htmx-test'});
+    res.render('index.html', {name});
 });
 
-app.get('/hello', (req, res) => {
-    res.render('hello', {name: 'hello page'});
+app.get('/test-page-1', (req, res) => {
+    res.render('test-page-1.html', {name});
 });
 
-app.get('/clicked', (req, res) => {
-    res.send(`<h1>name changed!<h2/>`);
+app.get('/test-page-2', (req, res) => {
+    res.render('test-page-2.html', {name});
+});
+
+app.get('/load-data', (req, res) => {
+    res.send(
+        new Array(100).fill(0).map(() => `
+            <li>
+               new data
+            </li>
+        `).join('')
+    );
 });
 
 app.listen(port, () => {
